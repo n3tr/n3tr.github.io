109335041da3f114d1559962424c2a2503612304
Jirat Ki <saakyz@gmail.com>
2018-05-14 14:55:28 +0700

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter Blog',
+    author: 'Kyle Mathews',
+    description: 'A starter blog demonstrating what Gatsby can do.',
+    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+  },
+  pathPrefix: '/gatsby-starter-blog',
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+    {
+      resolve: `gatsby-source-youtube`,
+      options: {
+        channelId: 'UC2e64YsO3vH8LAP7OJxfmdQ',
+        apiKey: 'AIzaSyAccJ8PhQvpLMk-4EBpjZBHiZtIsMuDlPU',
+        maxVideos: 50 // Defaults to 50
+      },
+    }
+  ],
+}
